e8225fc484d6e816f514e9cabb18ceeddaf4fbac
parveen-sishodiya-bitbucket <parveen.sishodiya.official@gmail.com>
2020-10-23 13:28:14 +0530

    Reverse String without java reverse API

---
+++ java/string/reverse-string/solutions/O(n)/ReverseString.java
@@ -0,0 +1,35 @@
+/******************************************************************************
+
+Author List:
+	Parveen
+
+Sample I/O:
+	Input: 'stardum'
+	Output: 'mudrats'
+	
+Complexity:
+	Time Complexity: O(n)
+	Space Complexity: O(n) 
+
+*******************************************************************************/
+
+public class ReverseString
+{
+	public static void main(String[] args) {
+	    String str = "abcdefgh";
+		System.out.println(getReversedString(str));
+	}
+	
+	public static String getReversedString(String str){
+	    int strLen = str.length();
+	    char[] strArray = str.toCharArray();
+	    char temp;
+	    for(int i = 0; i <= ((strLen-1)/2); i++){
+	       temp = strArray[i];
+	       strArray[i] = strArray[strLen - 1 - i];
+	       strArray[strLen - 1 - i] = temp;
+	    }
+	    return String.valueOf(strArray);
+	}
+	
+}
